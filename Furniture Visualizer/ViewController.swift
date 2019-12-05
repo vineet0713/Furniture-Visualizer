ad6f16722b4d478342e98448bc9c1cac9e05f81a
Vineet Joshi <vineet0713@gmail.com>
2019-12-05 10:19:19 -0800

    Initial Commit

---
+++ Furniture Visualizer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Furniture Visualizer
+//
+//  Created by Vineet Joshi on 12/5/19.
+//  Copyright © 2019 Vineet Joshi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
