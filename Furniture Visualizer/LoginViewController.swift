f736a31ff7beb93b136958cd37be19b0eee7165c
Vineet Joshi <vineet0713@gmail.com>
2019-12-17 12:38:25 -0800

    Finish UI for login screen.

--- Furniture Visualizer/LoginViewController.swift
+++ Furniture Visualizer/LoginViewController.swift
@@ -10,11 +10,17 @@ import UIKit
 
 class LoginViewController: UIViewController {
     
+    // MARK: - Properties
+    
+    let topLabel = UILabel()
+    let loginButton = UIButton()
+    
     // MARK: - UIViewController Life Cycle Functions
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        setupTopLabel()
         setupLoginButton()
     }
     
@@ -23,13 +29,40 @@ class LoginViewController: UIViewController {
 // MARK: - Extension: Setup the UI
 extension LoginViewController {
     
+    func setupTopLabel() {
+        topLabel.text = "Furniture Visualizer"
+        topLabel.font = UIFont.systemFont(ofSize: 50)
+        topLabel.textAlignment = .center
+        topLabel.adjustsFontSizeToFitWidth = true
+        view.addSubview(topLabel)
+        addConstraintsToTopLabel()
+    }
+    
+    func addConstraintsToTopLabel() {
+        topLabel.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            topLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            topLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20),
+            topLabel.widthAnchor.constraint(equalTo: view.safeAreaLayoutGuide.widthAnchor, multiplier: 0.75)
+        ])
+    }
+    
     func setupLoginButton() {
-        let loginButton = UIButton(frame: CGRect(x: 100, y: 100, width: 100, height: 50))
+        loginButton.titleLabel?.font = UIFont.systemFont(ofSize: 24)
         let defaultColor = UIColor(red: 0.0, green: 122.0/255.0, blue: 1.0, alpha: 1.0)
         loginButton.setTitleColor(defaultColor, for: .normal)
         loginButton.setTitle("Login", for: .normal)
         loginButton.addTarget(self, action: #selector(loginButtonTapped), for: .touchUpInside)
         view.addSubview(loginButton)
+        addConstraintsToLoginButton()
+    }
+    
+    func addConstraintsToLoginButton() {
+        loginButton.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            loginButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            loginButton.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+        ])
     }
     
 }
